20a1053f50330b32b16cf54048db59dae0dce67f
Nox1do <noogux@gmail.com>
2022-09-06 20:32:58 +0000

    updating card element

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -8,7 +8,7 @@ const Card = () => {
 	return (
 		<div className="container">
   <div className="row">
-    <div className="col-md-4">
+    <div className="col-md-3">
       <div className="card">
         <img
           src="https://cdn.pixabay.com/photo/2019/03/01/18/52/house-4028391__340.jpg"
@@ -26,7 +26,7 @@ const Card = () => {
         </div>
       </div>
     </div>
-    <div className="col-md-4">
+    <div className="col-md-3">
       <div className="card">
         <img
           src="https://cdn.pixabay.com/photo/2019/02/25/19/22/brownie-4020342__340.jpg"
@@ -45,7 +45,7 @@ const Card = () => {
         
       </div>
     </div>
-    <div className="col-md-4">
+    <div className="col-md-3">
       <div className="card">
         <img
           src="https://cdn.pixabay.com/photo/2019/03/02/10/10/swan-4029559__340.jpg"
@@ -63,6 +63,26 @@ const Card = () => {
         </div>
       </div>
     </div>
+    
+    <div className="col-md-3">
+      <div className="card">
+        <img
+          src="https://cdn.pixabay.com/photo/2022/08/28/18/03/dog-7417233_960_720.jpg"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <hr/>
+          <button type="button" class="btn btn-dark justify-center">Find out more</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </div>
 
